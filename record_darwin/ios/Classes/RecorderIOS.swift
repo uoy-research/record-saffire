0c419c0fe5e94c81e9eebdf3f84c159bc41ab779
Philip Harrison <philip.harrison@york.ac.uk>
2024-04-18 11:39:30 +0100

    Changes Bluetooth profile to A2DP
    This prevents the bluetooth profile switching to HFP resulting in mono audio and using the bluetooth microphone.

--- record_darwin/ios/Classes/RecorderIOS.swift
+++ record_darwin/ios/Classes/RecorderIOS.swift
@@ -12,7 +12,7 @@ func listInputs() throws -> [Device] {
 
 func listInputDevices() throws -> [AVAudioSessionPortDescription]? {
   let audioSession = AVAudioSession.sharedInstance()
-  let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .allowBluetooth, .mixWithOthers]
+  let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .allowBluetoothA2DP, .mixWithOthers]
   
   do {
     try audioSession.setCategory(.playAndRecord, options: options)
@@ -46,7 +46,7 @@ private func setInput(_ config: RecordConfig) throws {
 extension AudioRecordingDelegate {
   func initAVAudioSession(config: RecordConfig) throws {
     let audioSession = AVAudioSession.sharedInstance()
-    let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .allowBluetooth, .mixWithOthers]
+    let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .allowBluetoothA2DP, .mixWithOthers]
     
     do {
       try audioSession.setCategory(.playAndRecord, options: options)
